6cb750e078ef675f89939a0e9bfc0186be160a96
markivx <markiv915@gmail.com>
2017-05-20 21:35:00 -0700

    limber
    Signed-off-by: markivx <markiv915@gmail.com>

--- gitgl.py
+++ gitgl.py
@@ -14,14 +14,16 @@ repostr = sys.argv[1] + "/.git"
 repo = Repository(repostr)
 vertices = []
 colors = []
-
-color_mergelines = [0.7, 0.7, 0.7]
-color_commitlines = [0.50, 0.50, 0.80]
-
+color_mergelines =  [0.25, 0.25, 0.75]
+color_commitlines = [0.30, 0.30, 0.60]
+color_commitbox = [0.60, 0.60, 0.60]
 count = 0
 x = 0
 y = 2
 z = 0
+clen = 0.40
+nextlen = 5.00
+visitedcommits = {'XXXX': 0}
 
 def drawText(font, mystr, x, y, z):
 	tsf = font.render(mystr, True, (255, 255, 255, 0), (0, 0, 0, 255))
@@ -40,15 +42,10 @@ glTranslatef(0.0, 0.0, -5)
 selfont = pygame.font.match_font('ubuntucondensed')
 font = pygame.font.Font(selfont, 18)
 
-textlist = glGenLists(1)
-glNewList (textlist, GL_COMPILE)
-commit = repo.revparse_single('HEAD')
+commit = repo.revparse_single(sys.argv[2])
 repohead = commit
 random.seed(repohead.hex)
 
-clen=0.07
-nextlen = 5.00
-visitedcommits = {'XXXX': 0}
 
 class viscommit:
 	def __init__(self, commitobj, x, y, z, px, py, pz):
@@ -65,10 +62,10 @@ def add_commit(colors, vertices, x, y, z, commit, clen, rendertext):
 	colors.extend(color_mergelines)
 	colors.extend(color_mergelines)
 	colors.extend(color_mergelines)
-	colors.extend(color_mergelines)
-	colors.extend(color_mergelines)
-	colors.extend(color_mergelines)
-	colors.extend(color_mergelines)
+	colors.extend(color_commitbox)
+	colors.extend(color_commitbox)
+	colors.extend(color_commitbox)
+	colors.extend(color_commitbox)
 
 	# Draw commit symbol
 	vertices.extend([x - clen/2, y, z])
@@ -87,16 +84,16 @@ def add_commit(colors, vertices, x, y, z, commit, clen, rendertext):
 		drawText(font, text, x + 0.02, y + 0.03, z)
 
 	if len(commit.parents) == 0:
-		return colors, vertices
+		return colors, vertices, 0
 
-
-	colors.extend([0.50, 0.50, 0.80])
-	colors.extend([0.50, 0.50, 0.80])
+	randy = random.uniform(5, 15)
+	colors.extend(color_commitlines)
+	colors.extend(color_commitlines)
 	vertices.extend([x, y - clen, z])
-	vertices.extend([x, y - nextlen, z])
+	vertices.extend([x, y - nextlen - randy, z])
 	if commit.parents[0].hex in visitedcommits:
-		drawText(font, commit.parents[0].hex[0:7], x + 0.02, y - clen - nextlen, z)
-	return colors, vertices
+		drawText(font, commit.parents[0].hex[0:7], x + 0.02, y - clen - nextlen - randy, z)
+	return colors, vertices, randy
 
 v = viscommit(commit, x, y, z, x, y, z)
 orighead = copy.copy(v)
@@ -117,6 +114,8 @@ def printprogress(commitscount):
 	if commitscount % 25 == 0:
 		print "total: ", commitscount, "commits per second: ", commitscount/(curtime - starttime)
 
+textlist = glGenLists(1)
+glNewList (textlist, GL_COMPILE)
 while len(mergestack) > 0:
 	vc = mergestack.pop()
 	commit = vc.commitobj
@@ -129,7 +128,7 @@ while len(mergestack) > 0:
 	colors.extend(color_mergelines)
 	vertices.extend([x - clen/2, y - clen/2, z])
 	vertices.extend([vc.parentx + clen/2, vc.parenty - clen/2, vc.parentz])
-	colors, vertices = add_commit(colors, vertices, x, y, z, commit, clen, True)
+	colors, vertices, randy = add_commit(colors, vertices, x, y, z, commit, clen, True)
 	#if commit.hex in visitedcommits:
 	#	continue
 	visitedcommits[commit.hex] = 1
@@ -140,33 +139,32 @@ while len(mergestack) > 0:
 		if switch == 1:
 			pxs = x + xlim + r
 		else:
-			pxs = x - xlim - r
+			pxs = x - (xlim + r)
 		if len(commit.parents) > 1:
 			for i, parent in enumerate(commit.parents):
+				r = random.uniform(5, 10)
 				if i == 0:
 					continue
-				vc = viscommit(parent, pxs, y, z, x, y, z)
 				if switch == 1:
-					pxs = pxs + xlimextend
+					pxs = pxs + xlimextend + r
 				else:
-					pxs = pxs - xlimextend
+					pxs = pxs - (xlimextend + r)
+				vc = viscommit(parent, pxs, y, z, x, y, z)
 				mergestack.append(vc)
 			switch = 1 - switch
 			xlim = xlim + xlimextend
 			if xlim >= 5.0:
 				xlim = xlimextend
-		y = y - (nextlen)
+		y = y - (nextlen) - randy
 
 		commit = commit.parents[0]
 		if commit.hex in visitedcommits:
 			break
 		#print commit.hex,commit.message.split("\n")[0]
-		colors, vertices = add_commit(colors, vertices, x, y, z, commit, clen, False)
+		(colors, vertices, randy) = add_commit(colors, vertices, x, y, z, commit, clen, False)
 		visitedcommits[commit.hex] = 1
 		printprogress(commitscount)
 		commitscount = commitscount + 1
-
-		
 glEndList ()
 glShadeModel (GL_FLAT)
 
