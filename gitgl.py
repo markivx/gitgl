0f31a441aad52ae1a52b35c0a49e116debee041c
markivx <markiv915@gmail.com>
2017-05-22 07:39:32 -0700

    its a freakin indignity Tucker
    Signed-off-by: markivx <markiv915@gmail.com>

--- gitgl.py
+++ gitgl.py
@@ -15,8 +15,8 @@ repo = Repository(repostr)
 vertices = []
 colors = []
 color_mergelines =  [0.05, 0.05, 0.55]
-color_commitlines = [0.30, 0.30, 0.60]
-color_commitbox = [0.90, 0.90, 0.90]
+color_commitlines = [0.20, 0.20, 0.50]
+color_commitbox = [0.93, 0.93, 0.93]
 count = 0
 x = 0
 y = 2
@@ -43,7 +43,7 @@ glEnableClientState (GL_COLOR_ARRAY)
 gluPerspective(45, (display[0]/display[1]), 0.01, 9250.0)
 glTranslatef(0.0, 0.0, -50)
 selfont = pygame.font.match_font('ubuntucondensed')
-font = pygame.font.Font(selfont, 16)
+font = pygame.font.Font(selfont, 19)
 commit = repo.revparse_single(sys.argv[2])
 impcommithash = sys.argv[3]
 impcommit = repo.revparse_single(impcommithash)
@@ -51,8 +51,9 @@ repohead = commit
 random.seed(repohead.hex)
 
 class viscommit:
-	def __init__(self, commitobj, x, y, z, px, py, pz, mergecolor):
+	def __init__(self, commitobj, childcommitobj, x, y, z, px, py, pz, mergecolor):
 		self.commitobj = commitobj
+		self.childcommit = childcommitobj
 		self.parentx = px
 		self.parenty = py
 		self.parentz = pz
@@ -99,8 +100,8 @@ def add_commit(colors, vertices, x, y, z, commit, clen, rendertext, commitlinesc
 		drawText(font, commit.parents[0].hex[0:8], x + 0.02, y - clen - nextlen - randy, z, textcolor)
 	return colors, vertices, randy
 
-v = viscommit(commit, x, y, z, x, y, z, color_commitlines)
-orighead = copy.copy(v)
+v = viscommit(commit, None, x, y, z, x, y, z, color_commitlines)
+orighead = copy.copy(v).commitobj
 mergestack = [v]
 pxs = x
 switch = 0
@@ -126,7 +127,6 @@ while len(mergestack) > 0:
 	x = float(vc.x)
 	y = float(vc.y)
 	z = float(vc.z)
-	
 	colors.extend(vc.mergecolor)
 	colors.extend(vc.mergecolor)
 	colors.extend(vc.mergecolor)
@@ -135,16 +135,21 @@ while len(mergestack) > 0:
 	vertices.extend([x, vc.parenty - clen/2, vc.parentz])
 	vertices.extend([x, vc.parenty - clen/2, vc.parentz])
 	vertices.extend([vc.parentx, vc.parenty - clen/2, vc.parentz])
-	color_commitlines[2] = (float(40) + random.uniform(41, 255)) / 255
+	color_commitlines[2] = (float(125) + random.uniform(126, 225)) / 255
+
+	# if we've seen this before, draw the commit text and continue on to the next merge
+	if commit.hex in visitedcommits and commit.hex not in orighead.hex:
+		drawText(font, commit.parents[0].hex[0:8], x + 0.02, y - clen - nextlen - randy, z, textcolor)
+		continue
 	if firstline == True:
 		commitlinescolor = centrallinecolor
 	else:
 		commitlinescolor = color_commitlines
 	colors, vertices, randy = add_commit(colors, vertices, x, y, z, commit, clen, True, commitlinescolor)
 	if impcommit.hex in commit.hex:
-			visitedcommits[commit.hex] = (x, y, z, commit)
+		visitedcommits[commit.hex] = (x, y, z, commit, vc.childcommit, firstline)
 	else:
-			visitedcommits[commit.hex] = 1
+		visitedcommits[commit.hex] = (x, y, z, commit, vc.childcommit, firstline)
 	printprogress(commitscount)
 	commitscount = commitscount + 1
 	while len(commit.parents) > 0:
@@ -157,8 +162,12 @@ while len(mergestack) > 0:
 		if len(commit.parents) > 1:
 			ismerge = True
 			vcmergecolor = copy.copy(color_mergelines)
-			vcmergecolor[2] = (float(40) + random.uniform(41, 255)) / 255
+			vcmergecolor[2] = (float(40) + random.uniform(41, 225)) / 255
 			for i, parent in enumerate(commit.parents):
+				# We still want to know about merge parents even if we've seen them before
+				# Don't continue on these when popped from mergestack!
+				#if parent.hex in visitedcommits:
+				#	continue
 				r = random.uniform(5, 10)
 				q = random.uniform(25, 50)
 				if i == 0:
@@ -168,7 +177,7 @@ while len(mergestack) > 0:
 				else:
 					pxs = pxs - (xlimextend * r)
 				pxy = y - q
-				vc = viscommit(parent, pxs, pxy, z, x, y, z, vcmergecolor)
+				vc = viscommit(parent, commit, pxs, pxy, z, x, y, z, vcmergecolor)
 				mergestack.append(vc)
 			switch = 1 - switch
 			xlim = xlim + xlimextend
@@ -176,22 +185,66 @@ while len(mergestack) > 0:
 				xlim = xlimextend
 		y = y - (nextlen) - randy
 
+		childcommit = commit
 		commit = commit.parents[0]
+		#print commit.hex,commit.message.split("\n")[0]
 		if commit.hex in visitedcommits:
 			break
-		#print commit.hex,commit.message.split("\n")[0]
-		(colors, vertices, randy) = add_commit(colors, vertices, x, y, z, commit, clen, ismerge or (len(commit.parents) > 1), commitlinescolor)
+		(colors, vertices, randy) = add_commit(colors, vertices, x, y, z, commit, clen,
+							ismerge or (len(commit.parents) > 1),
+							commitlinescolor)
 		if impcommit.hex in commit.hex:
-			visitedcommits[commit.hex] = (x, y, z, commit)
+			visitedcommits[commit.hex] = (x, y, z, commit, childcommit, firstline)
 		else:
-			visitedcommits[commit.hex] = 1
+			visitedcommits[commit.hex] = (x, y, z, commit, childcommit, firstline)
 		printprogress(commitscount)
 		commitscount = commitscount + 1
+
+	if firstline == True:
+		(colors, vertices, randy) = add_commit(colors, vertices, x, y, z, commit, clen,
+							True,
+							commitlinescolor)
 	firstline = False
+
+implinescolor = [1, 1, 1]
+
+print "Starting highlight procedure..."
+traverse = visitedcommits[impcommit.hex]
+while traverse[5] is not True:
+	x = traverse[0]
+	y = traverse[1]
+	z = traverse[2]
+	child = visitedcommits[traverse[4].hex]
+	if child is None:
+		break
+
+	cx = child[0]
+	cy = child[1]
+	cz = child[2]
+
+	if x == cx:
+		colors.extend(implinescolor)
+		colors.extend(implinescolor)
+		vertices.extend([x, y, z])
+		vertices.extend([cx, cy, cz])
+	else:
+		# reached a merge point!
+		colors.extend(implinescolor)
+		colors.extend(implinescolor)
+		colors.extend(implinescolor)
+		colors.extend(implinescolor)
+		vertices.extend([x, y, z])
+		vertices.extend([x, cy - clen/2, z])
+		vertices.extend([x, cy - clen/2, z])
+		vertices.extend([cx, cy - clen/2, z])
+	traverse = visitedcommits[traverse[4].hex]
+print "Finished highlight procedure..."
 
 glEndList ()
 glShadeModel (GL_FLAT)
+print "Finished constructing text lists"
 
+print "Binding buffers..."
 # create vertex buffer object
 vbo = glGenBuffers (1)
 glBindBuffer (GL_ARRAY_BUFFER, vbo)
@@ -203,6 +256,7 @@ cbo = glGenBuffers(1)
 glBindBuffer(GL_ARRAY_BUFFER, cbo)
 glBufferData(GL_ARRAY_BUFFER, len(colors)*4, (c_float*len(colors))(*colors), GL_STATIC_DRAW)
 glColorPointer(3, GL_FLOAT, 0, None)
+print "....done. Number of vertices:", len(vertices)
 
 speed = 0.50
 running = True
@@ -249,21 +303,21 @@ while running:
 		kp = pygame.key.get_pressed()
 		if kp[K_RIGHT] or kp[K_d]:
  			glTranslatef(-1 * speed, 0, 0)
- 		elif kp[K_LEFT] or kp[K_a]:
+ 		if kp[K_LEFT] or kp[K_a]:
 			glTranslatef(speed, 0, 0)
-		elif kp[K_w]:
+		if kp[K_w]:
 			glTranslatef(0, 0,  speed)
-		elif kp[K_s]:
+		if kp[K_s]:
 			glTranslatef(0, 0, -1 * speed)
-		elif kp[K_DOWN]:
+		if kp[K_DOWN]:
 			glTranslatef(0, speed,  0)
-		elif kp[K_UP]:
+		if kp[K_UP]:
 			glTranslatef(0, -1 * speed, 0)
-		elif kp[K_PAGEDOWN]:
+		if kp[K_PAGEDOWN]:
 			glTranslatef(0, speed,  0)
-		elif kp[K_PAGEUP]:
+		if kp[K_PAGEUP]:
 			glTranslatef(0, -1 * speed, 0)
-		elif kp[K_z]:
+		if kp[K_z]:
 			running = False
 			break
 
